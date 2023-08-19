c90e33c7a28a8874f10a9094b9e186e039870017
Anchal Tanwar <86921001+anchaltanwar@users.noreply.github.com>
2023-08-19 10:59:23 +0530

    Update ErrorHandlingContract.sol

--- ErrorHandlingContract.sol
+++ ErrorHandlingContract.sol
@@ -5,23 +5,18 @@ contract errorhandling{
     uint public amount=0;
     uint public quotient=0;
 
-    //creating a function with name RequireCheck and parameters balance and coins 
     function check(uint balance, uint coins) public payable  {
         amount+=coins;
-        //using require function and giving it a condition
         require(balance>=10,"Sorry!! balance is less than 10. Transaction is not possible");
     }
 
     error Throw_error(string);
-    //creating another function 
     function RevertCheck(uint balance, uint coins)public payable  {
         amount+=coins;
-        //using if conditon and using revert function inside the if condition
         if(balance<10)
             revert Throw_error("Balance is less than 10. Transaction is not possible");
     }
 
-    //creating a function and using an assert function inside it
     function AssertCheck(uint i, uint j) public {
         assert(j!=0);
 
